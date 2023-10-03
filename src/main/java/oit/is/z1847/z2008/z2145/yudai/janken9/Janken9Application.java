71e7e1176afe2e95908edb0272a04afab7643437
e1b21099 <e1b21099@oit.ac.jp>
2023-10-03 16:33:07 +0900

    リポジトリの初期化

---
+++ src/main/java/oit/is/z1847/z2008/z2145/yudai/janken9/Janken9Application.java
@@ -0,0 +1,13 @@
+package oit.is.z1847.z2008.z2145.yudai.janken9;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Janken9Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Janken9Application.class, args);
+	}
+
+}
---
+++ src/test/java/oit/is/z1847/z2008/z2145/yudai/janken9/Janken9ApplicationTests.java
@@ -0,0 +1,13 @@
+package oit.is.z1847.z2008.z2145.yudai.janken9;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Janken9ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
